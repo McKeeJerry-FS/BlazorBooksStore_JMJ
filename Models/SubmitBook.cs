64510815011505afd3898bdf389028bf5b6539c6
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-05-27 22:05:16 -0400

    created a new model for Submitting a book.

---
+++ Models/SubmitBook.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorBooksStore.Models
+{
+    public class SubmitBook
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public string? ISBN { get; set; }
+        public int PagesCount { get; set; }
+        //TODO: Other properties to be added in the next sections
+    }
+}
