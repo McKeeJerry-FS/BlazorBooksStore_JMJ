3eb4b9e9a7e2295ef29101f84d74dc2b9fac5ebd
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-06-02 09:28:52 -0400

    Connected the Books main view to the API

--- Program.cs
+++ Program.cs
@@ -9,12 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiUrl = builder.Configuration["ApiUrl"];
-if (string.IsNullOrWhiteSpace(apiUrl))
-{
-	throw new InvalidOperationException("ApiUrl configuration is missing or empty.");
-}
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
 
 // Register for new services
 builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
