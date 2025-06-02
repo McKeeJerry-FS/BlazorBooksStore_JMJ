0aa062f2d53f5ad874608ca600feb0a3e75b6b88
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-06-02 13:01:35 -0400

    setup for managing HttpClients

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Http;
 using BlazorBooksStore.Services.Interfaces;
 using BlazorBooksStore.Services;
 using BlazorBooksStore;
 using Blazored.LocalStorage;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorBooksStore.Api"));
+
+builder.Services.AddHttpClient("BlazorBooksStore.Api", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiUrl"]);
+});
 
 // Register for new services
 builder.Services.AddScoped<ILoggingService, ConsoleLoggingService>();
