be05e4865d90d61dd336851b68661d9b93984335
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-06-04 14:14:30 -0400

    updates

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -20,11 +20,20 @@ namespace BlazorBooksStore.Services
             var response = await _httpClient.PostAsJsonAsync("authentication/login", requestModel);
             if (response.IsSuccessStatusCode)
             {
-                return await response.Content.ReadFromJsonAsync<LoginResponse>();
+                var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>();
+                if (loginResponse == null)
+                {
+                    throw new Exception("Failed to deserialize login response.");
+                }
+                return loginResponse;
             }
             else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
                 var error = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                if (error == null)
+                {
+                    throw new Exception("Failed to deserialize error response.");
+                }
                 throw new ApiResponseException(error);
             }
             else
